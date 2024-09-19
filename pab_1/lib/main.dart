43af2f7a2b501a9f3513f77321641007b10c8433
qwerty-21 <rahmatmaulanaakbar.21@gmail.com>
2024-09-19 14:13:19 +0700

    up

--- pab_1/lib/main.dart
+++ pab_1/lib/main.dart
@@ -14,4 +14,41 @@ void main() {
   String teks3 = teks1 + "" + teks2;
 
   print(teks3);
+
+  int umur = 21;
+  double tinggi = 1.67;
+  print("Umur : $umur Tahun");
+  print("Tinggi : $tinggi Meter");
+
+  var angka = 45;
+  var nama = "Rahmat";
+  print(angka.runtimeType);
+  print(nama.runtimeType);
+
+  //collection dart
+
+  //1.list
+
+  List<int> mylist = [1, 2, 3, 4, 5, 1];
+  print(mylist);
+  //2.set
+  Set<int> myset = new Set.from([1, 2, 3, 4, 5, 1]);
+  print(myset);
+  myset.add(6); //menambah set
+  print(myset);
+  myset.remove(2); //menghapus data dari dalam set
+  print(myset);
+
+  //map
+
+  var ibukota = {
+    'Jakarta': 'Indonesia',
+    'London': 'England',
+    'Tokyo': 'Japan',
+  };
+  print(ibukota['Jakrta']);
+  ibukota['New Delhi'] = 'India';
+  print(ibukota);
+  var mapkeys = ibukota.keys;
+  print(mapkeys);
 }
